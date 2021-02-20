15a605a3ae08b24d6b4abdf9e5b14113eab7e445
raikkoj2 <60006318+raikkoj2@users.noreply.github.com>
2021-02-20 21:06:54 +0200

    part3 alustusta
    deleted react app
    initiated node
    installed express

---
+++ Osa 3/part3/index.js
@@ -0,0 +1,31 @@
+const http = require('http')
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2020-01-10T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2020-01-10T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2020-01-10T19:20:14.298Z",
+      important: true
+    }
+  ]
+  
+  const app = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(notes))
+  })
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
